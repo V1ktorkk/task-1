9a3fb3a38575eb6e53c9502118d3173d4f311a46
V1ktorkk <ya-viktor44444@yandex.ru>
2025-12-08 12:29:26 +0300

    new task

--- server.js
+++ server.js
@@ -1,70 +1,37 @@
 const express = require("express");
-const path = require("path");
-const https = require("https");
+const sharp = require("sharp");
 
 const app = express();
 
-const LOGIN = "viktorkk1";
+app.get("/makeimage", (req, res) => {
+  const width = parseInt(req.query.width, 10) || 100;
+  const height = parseInt(req.query.height, 10) || 100;
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/login", (req, res) => {
-  res.send(LOGIN);
-});
-
-app.get("/id/:N", (req, res) => {
-  const userID = req.params.N;
-  const apiUrl = `https://nd.kodaktor.ru/users/${userID}`;
-
-  const options = {
-    method: "GET",
-    headers: {},
-  };
-
-  https
-    .get(apiUrl, options, (apiRes) => {
-      let data = "";
-
-      apiRes.on("data", (chunk) => {
-        data += chunk;
-      });
-
-      apiRes.on("end", () => {
-        try {
-          const jsonData = JSON.parse(data);
-
-          if (jsonData && jsonData.login) {
-            res.send(jsonData.login);
-          } else {
-            res.status(400).send("Поле login не найдено");
-          }
-        } catch (err) {
-          res.status(400).send("Ошибка парсинга JSON: " + err.message);
-        }
-      });
-    })
-    .on("error", (err) => {
-      res
-        .status(500)
-        .send("Ошибка при запросе к nd.kodaktor.ru: " + err.message);
+  sharp({
+    create: {
+      width: width,
+      height: height,
+      channels: 4,
+      background: { r: 255, g: 255, b: 255, alpha: 1 },
+    },
+  })
+    .png()
+    .toBuffer((err, data) => {
+      if (err) {
+        return res.status(500).send("Error generating image");
+      }
+      res.set("Content-Type", "image/png");
+      res.send(data);
     });
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Internal Server Error");
+app.get("/login", (req, res) => {
+  const login = "viktorkk1";
+  res.send(login);
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
-  console.log(`  http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
